ac1f0465bf2745a670a34d6bf4c359734314e10d
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-07-02 15:23:17 +0700

    validasi basic collection

--- validation_test.go
+++ validation_test.go
@@ -177,3 +177,42 @@ func TestCollection(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func TestBasicCollection(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type User struct {
+		Id        string    `validate:"required"`
+		Name      string    `validate:"required"`
+		Addresses []Address `validate:"required,dive"`
+		Hobbies   []string  `validate:"required,dive,required,min=3"`
+	}
+
+	validate := validator.New()
+	request := User{
+		Id:   "1",
+		Name: "Bayu",
+		Addresses: []Address{
+			{
+				City:    "Bogor",
+				Country: "Indonesia",
+			},
+			{
+				City:    "Depok",
+				Country: "Indonesia",
+			},
+		},
+		Hobbies: []string{
+			"Gaming",
+			"Coding",
+		},
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
